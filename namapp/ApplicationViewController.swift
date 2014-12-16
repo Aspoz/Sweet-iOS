90d02fe2b8dfae2366eac7cb1e40669432acceab
Inooid <justdrawcats@gmail.com>
2014-12-16 15:05:45 +0100

    Removed old shadow functions from ApplicationViewController

--- namapp/ApplicationViewController.swift
+++ namapp/ApplicationViewController.swift
@@ -20,31 +20,6 @@ class ApplicationViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    // Function to apply pre-built shadow for cells on an UIView
-    func applyPlainShadow(view: UIView) {
-        var layer = view.layer
-        
-        layer.shadowColor = UIColor.blackColor().CGColor
-        layer.shadowOffset = CGSize(width: 0, height: 2)
-        layer.shadowOpacity = 0.2
-        layer.shadowRadius = 4
-    }
-    
-    // Function to apply custom shadow on an UIView
-    func applyCustomShadow(view: UIView, shadowWidth: Int, shadowHeight: Int, radius: CGFloat) {
-        var layer = view.layer
-        
-        layer.shadowColor = UIColor.blackColor().CGColor
-        layer.shadowOffset = CGSize(width: shadowWidth, height: shadowHeight)
-        layer.shadowOpacity = 0.2
-        layer.shadowRadius = radius
-    }
-    
-    func removeShadow(view: UIView) {
-        view.layer.shadowOpacity = 0.0
-    }
-    
 
     /*
     // MARK: - Navigation
