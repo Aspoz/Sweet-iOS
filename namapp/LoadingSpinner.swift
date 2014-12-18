7f3b2bea4f8c095b58cde6b8d9ccd4a766cd13eb
Inooid <justdrawcats@gmail.com>
2014-12-18 11:05:59 +0100

    Add LoadingSpinner class

---
+++ namapp/LoadingSpinner.swift
@@ -0,0 +1,49 @@
+//
+//  ShowLoadingSpinner.swift
+//  namapp
+//
+//  Created by Boyd Dames on 17-12-14.
+//  Copyright (c) 2014 Jordi Wippert. All rights reserved.
+//
+
+import UIKit
+
+public class LoadingSpinner {
+    var container: UIView = UIView()
+    var loadingView: UIView = UIView()
+    var spinner: UIActivityIndicatorView = UIActivityIndicatorView()
+    
+    class var instance: LoadingSpinner {
+        struct Static {
+            static let instance: LoadingSpinner = LoadingSpinner()
+        }
+        return Static.instance
+    }
+    
+    func startLoadingSpinner(view: UIView) {
+        container.frame = view.frame
+        container.center = view.center
+
+        loadingView.frame = CGRectMake(0, 0, 80, 80)
+        loadingView.center = view.center
+        loadingView.backgroundColor = UIColor.colorWithRGBHexWithAlpha(0x444444, alpha: 0.7)
+        loadingView.clipsToBounds = true
+        loadingView.roundedCorners(20)
+
+        
+        spinner.frame = CGRectMake(0.0, 0.0, 40.0, 40.0);
+        spinner.activityIndicatorViewStyle =
+            UIActivityIndicatorViewStyle.WhiteLarge
+        spinner.center = CGPointMake(loadingView.frame.size.width / 2,
+            loadingView.frame.size.height / 2);
+        loadingView.addSubview(spinner)
+        container.addSubview(loadingView)
+        view.addSubview(container)
+        spinner.startAnimating()
+    }
+    
+    func stopLoadingSpinner() {
+        spinner.stopAnimating()
+        container.removeFromSuperview()
+    }
+}
