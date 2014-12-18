972879f16fc7ae7a405b9a25873d208941799c79
Inooid <justdrawcats@gmail.com>
2014-12-18 11:02:18 +0100

    Add success callback to get function() Replaced casesUrl() with getAllCases() for better description

--- namapp/ArrayController.swift
+++ namapp/ArrayController.swift
@@ -15,35 +15,37 @@ protocol ArrayControllerProtocol {
 class ArrayController {
     var delegate: ArrayControllerProtocol
     var backend = Backend()
+    
     init(delegate: ArrayControllerProtocol) {
         self.delegate = delegate
     }
-    
-    func completion(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void {
-        if(error != nil) {
-            println(error.localizedDescription)
-        }
-        var err: NSError?
-        var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSArray
-        if(err != nil) {
-            println("JSON Error \(err!.localizedDescription)")
-        }
-        let results: NSArray = jsonResult as NSArray
-        self.delegate.didReceiveAPIResults(jsonResult)
-    }
 
-    func get(path: String) {
+    func get(path: String, getSuccess: () -> Void) {
         let url = NSURL(string: path)
         let session = NSURLSession.sharedSession()
         var request:NSMutableURLRequest = NSMutableURLRequest(URL: url!)
         var token = backend.userToken()
         request.setValue("Token token=\(token)", forHTTPHeaderField: "Authorization")
-        let task = session.dataTaskWithRequest(request, completionHandler: completion)
+        let task = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+            if(error != nil) {
+                println(error.localizedDescription)
+            }
+            var err: NSError?
+            var jsonResult = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers, error: &err) as NSArray
+            if(err != nil) {
+                println("JSON Error \(err!.localizedDescription)")
+            }
+            let results: NSArray = jsonResult as NSArray
+            self.delegate.didReceiveAPIResults(jsonResult)
+            getSuccess()
+        })
         task.resume()
     }
     
-       func casesUrl() {
-        let urlPath = "http://178.62.204.157/cases"
-        get(urlPath)
+    func getAllCases(success: () -> Void) -> Void {
+        let urlPath = backend.BASE_URL + "/cases"
+        get(urlPath, getSuccess: { () -> Void in
+            success()
+        })
     }
 }
